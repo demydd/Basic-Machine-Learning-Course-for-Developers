7ea7d307df8f4a871cd6167b498759fd1102890e
demydd <demydd@gmail.com>
2019-07-03 22:32:47 +0300

    Scaling is added.

--- Distibution_demo.R
+++ Distibution_demo.R
@@ -7,14 +7,64 @@ data_all <- data.table(read.csv(paste(location, filename, sep = "")))
 vars <- c('AMT_INCOME_TOTAL',	'AMT_CREDIT',	'AMT_ANNUITY',	'AMT_GOODS_PRICE',	'REGION_POPULATION_RELATIVE',	'HOUR_APPR_PROCESS_START')
 data_vars <- data_all[, ..vars]
 
-data_all$AMT_INCOME_TOTAL
-check <- sort(data_all$AMT_INCOME_TOTAL)[1:(length(data_all$AMT_INCOME_TOTAL)-100)]
+#data_all$AMT_INCOME_TOTAL
+#check <- sort(data_all$AMT_INCOME_TOTAL)[1:(length(data_all$AMT_INCOME_TOTAL)-100)]
 
-x <- c(1,2,3)
+#x <- c(1,2,3)
 
+makeHistScaled = function( data_init
+                     ,data_norm
+                     ,variable
+                     ,qty
+                     ,top_items
+                     ,bins
+                     ,ylim
+                     ,conf_interval
+                     ,conf_interval_type
+                     ,dfcurve
+){
+
+  name <- colnames(data_init)[colnames(data_init) == variable]
+  data <- as.vector(unlist(data_init[, ..name]))
+  data <- data[order(data)]
+  data <- data[1:(length(data) - top_items)]
+  #print(length(data) - top_items)
+
+  par(mfrow=c(2, 2))
+  hist(data, breaks = bins, main = paste("Initial data, confidence =", conf_interval))
+  hist(scale(data, center = TRUE, scale = FALSE), breaks = bins, main = paste("Centered data, confidence =", conf_interval))
+
+  hist(scale(data, center = FALSE, scale = TRUE), breaks = bins, prob = TRUE, ylim=c(0,ylim), main = paste("Scaled data, confidence =", conf_interval))
+  if(dfcurve == 'yes') curve(dnorm(x, mean(scale(data, center = FALSE, scale = TRUE)), sd(scale(data, center = FALSE, scale = TRUE))), add=TRUE, col="darkblue", lwd=2)
+  hist(scale(data, center = TRUE, scale = TRUE), breaks = bins, prob = TRUE, ylim=c(0,ylim), main = paste("Centered & Scaled data, confidence =", conf_interval))
+  if(dfcurve == 'yes') curve(dnorm(x, mean(scale(data, center = TRUE, scale = TRUE)), sd(scale(data, center = TRUE, scale = TRUE))), add=TRUE, col="darkblue", lwd=2)
+
+  conf_position <- round((1 - conf_interval)/2 * qty, 0)
+
+  if (conf_interval_type != 'none'){
+
+    if(conf_interval_type == 'left') {
+      left_limit <- data_norm[order(data_norm)][conf_position]
+      abline(v = left_limit, col = "red", lwd = 2)
+    }else{
+      if(conf_interval_type == 'right'){
+        right_limit <- data_norm[order(data_norm)][qty - conf_position]
+        abline(v = right_limit, col = "red", lwd = 2)
+      }else{
+        left_limit <- data_norm[order(data_norm)][conf_position]
+        right_limit <- data_norm[order(data_norm)][qty - conf_position]
+
+        abline(v = left_limit, col = "red", lwd = 2)
+        abline(v = right_limit, col = "red", lwd = 2)
+      }
+    }
+
+  }
+
+}
 
 makeHist = function( data_init
-                    ,data
+                    ,data_norm
                     ,variable
                     ,top_items
                     ,qty
@@ -25,47 +75,51 @@ makeHist = function( data_init
                     ,dfcurve
                    ){
 
+  name <- colnames(data_init)[colnames(data_init) == variable]
+  data <- unlist(data_init[, ..name])
+  data <- data[order(data)]
+  data <- data[1:(length(data) - top_items)]
 
+  par(mfrow=c(3, 1))
+  hist(data, breaks = bins, main = paste("Initial histogram of conf.interval", conf_interval, "qty =", ifelse(qty > length(data), length(data), qty)))
+  hist(scale(data), breaks = bins, prob = TRUE, ylim=c(0,ylim), main = paste("Scaled histogram of conf.interval", conf_interval, "qty =", ifelse(qty > length(data), length(data), qty)))
+  if(dfcurve == 'yes') curve(dnorm(x, mean(data_norm), sd(data_norm)), add=TRUE, col="darkblue", lwd=2)
 
-  par(mfrow=c(2, 1))
-  hist(data_init, breaks = bins, main = paste("Initial histogram of conf.interval", conf_interval, "qty =", ifelse(qty > length(data_init), length(data_init), qty)))
-
-  hist(data, breaks = bins, prob = TRUE, ylim=c(0,ylim)
-       , main = paste("Histogram of conf.interval" , conf_interval, "qty =", qty))
+  hist(data_norm, breaks = bins, prob = TRUE, ylim=c(0,ylim)
+       , main = paste("Theoretical histogram of conf.interval" , conf_interval, "qty =", qty))
+  if(dfcurve == 'yes') curve(dnorm(x, mean(data_norm), sd(data_norm)), add=TRUE, col="darkblue", lwd=2)
 
   conf_position <- round((1 - conf_interval)/2 * qty, 0)
 
   if (conf_interval_type != 'none'){
 
     if(conf_interval_type == 'left') {
-      left_limit <- data[order(data)][conf_position]
+      left_limit <- data_norm[order(data_norm)][conf_position]
       abline(v = left_limit, col = "red", lwd = 2)
     }else{
       if(conf_interval_type == 'right'){
-        right_limit <- data[order(data)][qty - conf_position]
+        right_limit <- data_norm[order(data_norm)][qty - conf_position]
         abline(v = right_limit, col = "red", lwd = 2)
       }else{
-        left_limit <- data[order(data)][conf_position]
-        right_limit <- data[order(data)][qty - conf_position]
+        left_limit <- data_norm[order(data_norm)][conf_position]
+        right_limit <- data_norm[order(data_norm)][qty - conf_position]
 
         abline(v = left_limit, col = "red", lwd = 2)
         abline(v = right_limit, col = "red", lwd = 2)
       }
     }
 
-
-
   }
-
-  if(dfcurve == 'yes') curve(dnorm(x, mean(data), sd(data)), add=TRUE, col="darkblue", lwd=2)
 
 }
 
 #debugonce(makeHist)
+xxx <- colnames(data_all)
+vars <- as.list(c('AMT_INCOME_TOTAL',	'AMT_CREDIT',	'AMT_ANNUITY',	'AMT_GOODS_PRICE',	'REGION_POPULATION_RELATIVE',	'HOUR_APPR_PROCESS_START'))
 
 manipulate(
 
-  variable = picker(colnames(data_all)),
+  variable = picker(vars),
   top_items = slider(1, 1000),
   qty = slider(1, 1000000),
   bins = slider(1,100),
@@ -76,8 +130,38 @@ manipulate(
 
   makeHist(data_all, rnorm(qty), variable, top_items
            , qty, bins, ylim, conf_interval, conf_interval_type, dfcurve)
-
 )
+
+#debugonce(makeHistScaled)
+
+manipulate(
+
+  variable = picker(vars),
+  top_items = slider(1, 1000),
+  qty = slider(1, 1000000),
+  bins = slider(1,100),
+  ylim = slider(0,1),
+  conf_interval_type = picker('none', 'left', 'right', 'both'),
+  conf_interval = picker(0.68, 0.9, 0.95, 0.99),
+  dfcurve = picker('no', 'yes'),
+
+  makeHistScaled( data_all
+                 ,rnorm(qty)
+                 ,variable
+                 ,qty
+                 ,top_items
+                 ,bins
+                 ,ylim
+                 ,conf_interval
+                 ,conf_interval_type
+                 ,dfcurve
+                )
+)
+
+
+variable <- 'AMT_INCOME_TOTAL'
+name <- colnames(data_all)[colnames(data_all) == variable]
+data_all[,]
 
 
 dev.off()
